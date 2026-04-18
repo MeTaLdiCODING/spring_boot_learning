a58bd72180447ca7a75c4d9730f3309eacdd2039
MeTaLdiCODING <daniilterehov353@gmail.com>
2026-04-18 22:06:25 +0300

    Get Logger

--- src/main/java/spring_boot_learning/sb_learn/ReservationController.java
+++ src/main/java/spring_boot_learning/sb_learn/ReservationController.java
@@ -1,14 +1,18 @@
 package spring_boot_learning.sb_learn;
 
+import org.apache.juli.logging.Log;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 public class ReservationController {
 
+private static final Logger log = (Logger) LoggerFactory.getLogger(ReservationController.class);
 
     public final ReservationService reservationService;
 
@@ -20,13 +24,13 @@ public class ReservationController {
     public Reservation getReservationById(
            @PathVariable("id") Long id
     ){
-        System.out.println("log called method ");
+       log.info("Called getReservationById: id= "+id);
         return  reservationService.getReservationById(id);
     }
 
     @GetMapping()
     public List<Reservation> getAllReservation() {
-        System.out.println("log called method ");
+        log.info("Called getAllReservation");
         return reservationService.findAllReservations();
     }
 
